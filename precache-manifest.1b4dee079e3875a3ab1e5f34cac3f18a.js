f8ad730c5a93c62da5b001d48f1a46572d00ccd5
larsdolvik <larsdolvik@outlook.com>
2019-12-01 01:49:21 +0100

    Updates

---
+++ precache-manifest.1b4dee079e3875a3ab1e5f34cac3f18a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e808d4a2e1dc8ca56d72aa57ceb7999f",
+    "url": "/julekalender/index.html"
+  },
+  {
+    "revision": "468095aaeca39daf43c0",
+    "url": "/julekalender/static/css/main.ced8a681.chunk.css"
+  },
+  {
+    "revision": "386672b1df73c406ee0b",
+    "url": "/julekalender/static/js/2.3fd113f3.chunk.js"
+  },
+  {
+    "revision": "468095aaeca39daf43c0",
+    "url": "/julekalender/static/js/main.4bc3750e.chunk.js"
+  },
+  {
+    "revision": "0d17d7d24164f4a44249",
+    "url": "/julekalender/static/js/runtime-main.f48f3edb.js"
+  },
+  {
+    "revision": "5fab84c99335c7afc8d53510cd9e1bc2",
+    "url": "/julekalender/static/media/_grassfield.5fab84c9.png"
+  },
+  {
+    "revision": "ffdc0275447ae2835cb6a9b9d634d37a",
+    "url": "/julekalender/static/media/_grassfieldDirt.ffdc0275.png"
+  },
+  {
+    "revision": "8699974be09775bb53eafe98340bc12d",
+    "url": "/julekalender/static/media/factory001.8699974b.png"
+  },
+  {
+    "revision": "e1ae6f9dd72990d7293344c3d3ae15ce",
+    "url": "/julekalender/static/media/factory002.e1ae6f9d.png"
+  },
+  {
+    "revision": "ce47d7f7b3caeee7d27ca8029151e290",
+    "url": "/julekalender/static/media/gift.ce47d7f7.svg"
+  },
+  {
+    "revision": "06eb5e330879b8b318e3a329030aaa6b",
+    "url": "/julekalender/static/media/heart.06eb5e33.svg"
+  },
+  {
+    "revision": "8fff35cc5ae7610b8f05af1f3ef91ab3",
+    "url": "/julekalender/static/media/treeFar002.8fff35cc.png"
+  },
+  {
+    "revision": "80f52742814e91e5ba5bfbdbc110a62c",
+    "url": "/julekalender/static/media/treeNear001.80f52742.png"
+  },
+  {
+    "revision": "77707ef4353d71f09e06acfe9eddafab",
+    "url": "/julekalender/static/media/treeNear002.77707ef4.png"
+  }
+]);
